1f8c0c12ff15f912d48da9a9ea0d5a5d61442f2b
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-08 17:42:28 +0300

    UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,41 +17,98 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: const _CustomAppBar(),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.deepPurple.shade800,
-          unselectedItemColor: Colors.white,
-          selectedItemColor: Colors.white,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+        bottomNavigationBar: _CustomNavBar(),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              _DiscoverMusic(),
+            ],
           ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline),
-              label: 'Favourites',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.play_circle_outline),
-              label: 'Play',
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.people_outline),
-              label: 'Profile',
-          ),
-          ],
         ),
-        body: Container(),
+      ),
+    );
+  }
+
+  BottomNavigationBar _CustomNavBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.deepPurple.shade800,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_outline),
+          label: 'Favourites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.play_circle_outline),
+          label: 'Play',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people_outline),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
+
+class _DiscoverMusic extends StatelessWidget {
+  const _DiscoverMusic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Welcome',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            'Enjoy your favourite music',
+            style: Theme.of(context)
+                .textTheme
+                .headline6!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+                isDense: true,
+                filled: true,
+                fillColor: Colors.white,
+                hintText: 'Search',
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.grey.shade500),
+                prefixIcon: Icon(Icons.search, color: Colors.grey.shade400),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide.none,
+                )),
+          )
+        ],
       ),
     );
   }
 }
 
-class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
+class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const _CustomAppBar({
-    Key ? key,
+    Key? key,
   }) : super(key: key);
 
   @override
@@ -59,20 +116,20 @@ class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: const Icon(Icons.grid_view_rounded), 
+      leading: const Icon(Icons.grid_view_rounded),
       actions: [
         Container(
           margin: const EdgeInsets.only(right: 20),
           child: const CircleAvatar(
-            backgroundImage: NetworkImage("https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"),
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"),
           ),
         )
       ],
     );
   }
-  
+
   @override
   // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(56.0);
 }
-

64c2d19914e88a601f98e4c7f19d40d16e380e1c
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-10 19:02:23 +0300

    songScreen

--- lib/screens/song_screen.dart
+++ lib/screens/song_screen.dart
@@ -1,13 +1,102 @@
+import 'package:dynamic_music_player/models/song_model.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart' as rxdart;
 
-class SongScreen extends StatelessWidget {
+import '../widgets/seekbar.dart';
+
+class SongScreen extends StatefulWidget {
   const SongScreen({super.key});
 
   @override
+  State<SongScreen> createState() => _SongScreenState();
+}
+
+class _SongScreenState extends State<SongScreen> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  Song song = Song.songs[0];
+
+  @override
+  void initState() {
+    super.initState();
+
+    audioPlayer.setAudioSource(ConcatenatingAudioSource(children: [
+      AudioSource.uri(
+        Uri.parse('asset:///${song.url}'),
+      ),
+    ]));
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Stream<SeekBarData> get _seekBarDataStream =>
+      rxdart.Rx.combineLatest2<Duration, Duration?, SeekBarData>(
+          audioPlayer.positionStream, audioPlayer.durationStream,
+          (Duration position, Duration? duration) {
+        return SeekBarData(
+          position,
+          duration ?? Duration.zero,
+        );
+      });
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Song Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            song.coverUrl,
+            fit: BoxFit.cover,
+          ),
+          const _BackgroundFilter()
+        ],
+      ),
+    );
+  }
+}
+
+class _BackgroundFilter extends StatelessWidget {
+  const _BackgroundFilter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) {
+        return LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.white.withOpacity(0.5),
+              Colors.white.withOpacity(0.0),
+            ],
+            stops: const [
+              0.0,
+              0.4,
+              0.6
+            ]).createShader(rect);
+      },
+      blendMode: BlendMode.dstOut,
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Colors.deepPurple.shade200,
+              Colors.deepPurple.shade800
+            ])),
       ),
     );
   }
---
+++ lib/widgets/seekbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SeekBarData {
+  final Duration position;
+  final Duration duration;
+
+  SeekBarData(this.position, this.duration);
+}
+
+class SeekBar extends StatefulWidget {
+  final Duration position;
+  final Duration duration;
+  final ValueChanged<Duration>? onChanged;
+  final ValueChanged<Duration>? onChangedEnd;
+
+  const SeekBar({
+    Key? key,
+    required this.duration,
+    this.onChanged,
+    this.onChangedEnd,
+    required this.position,
+  }) : super(key: key);
+
+  @override
+  State<SeekBar> createState() => _SeekBarState();
+}
+
+class _SeekBarState extends State<SeekBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
